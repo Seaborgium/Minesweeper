8fb17029d6d217d6aea9cdb6eeb76ecc386c9ae5
m4r71n85 <martin85@abv.bg>
2013-05-08 23:59:35 +0300

    ConsoleMinesweeperGame: refactoring code, not completed but it works

--- ConsoleMinesweeperGame.cs
+++ ConsoleMinesweeperGame.cs
@@ -4,9 +4,13 @@ namespace MinesweeperProject
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using System.Text.RegularExpressions;
 
     internal class ConsoleMinesweeperGame : MinesweeperGame
     {
+        private string commandLine;
+        private bool playGame = true;
+        private StringBuilder renderBuffer = new StringBuilder();
         public ConsoleMinesweeperGame(int rows, int columns, int minesCount)
             : base(rows, columns, minesCount)
         {
@@ -15,84 +19,108 @@ namespace MinesweeperProject
         public override void Start()
         {
             base.Start();
-            string startMessage = "Welcome to the game “Minesweeper”. Try to reveal all cells without mines. Use 'top' to view the scoreboard, 'restart' to start a new game and 'exit' to quit the game.";
-            Console.WriteLine(startMessage);
+            string greetingMessage = "Welcome to the game “Minesweeper”. Try to reveal all cells without mines. Use 'top' to view the scoreboard, 'restart' to start a new game and 'exit' to quit the game.";
+            Console.WriteLine(greetingMessage);
             Console.WriteLine(Grid.ToString());
-            this.NextCommand();
+
+            while (playGame)
+            {
+                this.ReadCommand();
+                this.ExecuteCommand();
+                this.Render();
+            }
         }
 
-        public void NextCommand() ////console -  output grid and message to request command
+        public void ReadCommand() ////console -  output grid and message to request command
         {
             Console.Write("Enter row and column:");
+            this.commandLine = Console.ReadLine().ToUpper().Trim();
+        }
 
-            string commandLine = Console.ReadLine().ToUpper().Trim();
+        private void ExecuteCommand()
+        {
+            string coordinatesPattern = @"\d+\s\d+";
+            bool isCoordinate = Regex.IsMatch(this.commandLine, coordinatesPattern);
 
-            List<string> commandList = commandLine.Split(' ').ToList();
-
-            if (commandList.Count == 0)
+            if (isCoordinate)
             {
-                this.NextCommand();
+                ExecuteCoordCommand();
             }
+            else
+            {
+                ExecuteMenuCommand();
+            }
+        }
+
+
+        private void ExecuteCoordCommand()
+        {
+            int row = 0;
+            int column = 0;
+            bool tryParse;
+            string[] coordinates = commandLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            tryParse = int.TryParse(coordinates[0], out row);
+            tryParse = int.TryParse(coordinates[1], out column) && tryParse;
+
+            if (!tryParse)
+            {
+                throw new CommandUnknownException();
+            }
+
+            if (Grid.RevealCell(row, column) == '*')
+            {
+                Grid.MarkCell('-');
+                Grid.RevealMines();
+                this.renderBuffer.AppendLine(Grid.ToString());
+                this.renderBuffer.AppendLine(string.Format("Booooom! You were killed by a mine. You revealed {0} cells without mines.", this.Score));
+                this.renderBuffer.AppendLine("Please enter your name for the top scoreboard: ");
+
+                string playerName = Console.ReadLine();
+                ScoreBoard.Add(new ScoreRecord(playerName, Score));
+                Console.WriteLine();
+
+                this.PrintScoreBoard();
+                this.Start();
+            }
+            else
+            {
+                Console.WriteLine(Grid.ToString());
+                this.Score++;
+                this.ReadCommand();
+            }
+        }
+
+
+        private void ExecuteMenuCommand()
+        {
+            //string[] command = commandLine.Split(new [] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            //if (this.commandList.Count < 2)
+            //{
+            //    throw new CommandUnknownException();
+            //}
+            //if (commandList.Count == 0)
+            //{
+            //    this.ReadCommand();
+            //}
 
             try
             {
-                string firstCommand = commandList.ElementAt(0);
-                switch (firstCommand)
+                switch (this.commandLine)
                 {
                     case "RESTART":
                         this.Start();
                         break;
                     case "TOP":
                         this.PrintScoreBoard();
-                        this.NextCommand();
                         break;
                     case "EXIT":
-                        this.Exit();
+                        Console.WriteLine("Good bye!");
                         break;
-                    ////case "EXPLOREMINES":
-                    ////    {
-                    ////        Grid.RevealMines();
-                    ////        Console.WriteLine(Grid.ToString());
-                    ////        NextCommand();
-                    ////    }; break;
-                    default:
+                    case "CHEAT":
                         {
-                            int row = 0;
-                            int column = 0;
-                            bool tryParse = false;
-
-                            if (commandList.Count < 2)
-                            {
-                                throw new CommandUnknownException();
-                            }
-
-                            tryParse = int.TryParse(commandList.ElementAt(0), out row) || tryParse;
-                            tryParse = int.TryParse(commandList.ElementAt(1), out column) || tryParse;
-
-                            if (!tryParse)
-                            {
-                                throw new CommandUnknownException();
-                            }
-
-                            if (Grid.RevealCell(row, column) == '*')
-                            {
-                                Grid.MarkCell('-');
-                                Grid.RevealMines();
-                                Console.WriteLine(Grid.ToString());
-                                Console.WriteLine(string.Format("Booooom! You were killed by a mine. You revealed {0} cells without mines.", this.Score));
-                                Console.Write("Please enter your name for the top scoreboard: ");
-                                string playerName = Console.ReadLine();
-                                ScoreBoard.Add(new ScoreRecord(playerName, Score));
-                                Console.WriteLine();
-                                this.PrintScoreBoard();
-                                this.Start();
-                            }
-                            else
-                            {
-                                Console.WriteLine(Grid.ToString());
-                                this.Score++;
-                                this.NextCommand();
-                            }
+                            Grid.RevealMines();
+                            this.renderBuffer.AppendLine(Grid.ToString());
                         }
 
                         break;
@@ -101,33 +129,31 @@ namespace MinesweeperProject
             catch (InvalidCellException)
             {
                 Console.WriteLine("Illegal move!");
-                this.NextCommand();
+                this.ReadCommand();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                this.NextCommand();
+                this.ReadCommand();
             }
-        }
-
-        public void Exit()
-        {
-            Console.WriteLine("Good bye!");
         }
 
         public void PrintScoreBoard()
         {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine("Scoreboard:");
+            this.renderBuffer.AppendLine("Scoreboard:");
             ScoreBoard.Sort();
             int i = 0;
             foreach (ScoreRecord scoreRecord in this.ScoreBoard)
             {
                 i++;
-                stringBuilder.AppendFormat("{0}. {1} --> {2} cells \n", i, scoreRecord.PlayerName, scoreRecord.Score);
+                this.renderBuffer.AppendFormat("{0}. {1} --> {2} cells \n", i, scoreRecord.PlayerName, scoreRecord.Score);
             }
+        }
 
-            Console.WriteLine(stringBuilder.ToString());
+        private void Render()
+        {
+            Console.WriteLine(renderBuffer);
+            renderBuffer.Clear();
         }
     }
 }
