ebe7d03819bb2d2ce9dd12dfd3a2081755953336
m4r71n85 <martin85@abv.bg>
2013-05-17 00:36:46 +0300

    Rename ScoreBoard to ScoreRecord and make some other cosmetic changes

---
+++ ScoreRecord.cs
@@ -0,0 +1,30 @@
+﻿namespace MinesweeperProject
+{
+    using System;
+    using System.Linq;
+
+    public class ScoreRecord : IComparable
+    {
+        public ScoreRecord(string playerName, int score)
+        {
+            this.PlayerName = playerName;
+            this.Score = score;
+        }
+
+        public string PlayerName { get; set; }
+
+        public int Score { get; set; }
+
+        public int CompareTo(Object obj)
+        {
+            if (!(obj is ScoreRecord))
+            {
+                throw
+                new ArgumentException("Compared Object is not ScoreRecord!");
+            }
+            ScoreRecord comparedScore = ((ScoreRecord)obj);
+            int compareResult = -1 * this.Score.CompareTo(comparedScore.Score);
+            return compareResult;
+        }
+    }
+}
