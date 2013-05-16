b1ca8e7768cc72dbf50c8228c9751675b4a887d1
Hristo Bratanov <hristo_brat@yahoo.com>
2013-05-16 21:09:25 +0300

    MinesweeperExceptions.cs refactured and documented.

--- MinesweeperExceptions.cs
+++ MinesweeperExceptions.cs
@@ -1,48 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace MinesweeperProject
+﻿namespace MinesweeperProject
 {
-    class InvalidCellException : Exception
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// Exception class used for throwing exceptions if the grid cell is invalid.
+    /// </summary>
+    class InvalidCellException : ApplicationException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidCellException" /> class,
+        /// with no parameters and message "Invalid cell!"
+        /// </summary>
         public InvalidCellException()
             : base("Invalid cell!")
         {
         }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidCellException" /> class,
+        /// with option for custom message!"
+        /// </summary>
+        /// <param name="message">custom message</param>
         public InvalidCellException(string message)
             : base(message)
         {
         }
     }
 
-    class IllegalMoveException : Exception
+    /// <summary>
+    /// Exception class used for throwing exceptions in case of illegal 
+    /// movement in the grid.
+    /// </summary>
+    class IllegalMoveException : ApplicationException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IllegalMoveException" /> class,
+        /// with no parameters and message "Illegal move!"
+        /// </summary>
         public IllegalMoveException()
             : base("Illegal move!")
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IllegalMoveException" /> class,
+        /// with option for custom message!"
+        /// </summary>
+        /// <param name="message">custom message</param>
         public IllegalMoveException(string message)
             : base(message)
         {
         }
     }
 
-    class CommandUnknownException : Exception
+    /// <summary>
+    /// Exception class used for throwing exceptions in case unknown 
+    /// command entered by the user.
+    /// </summary>
+    class CommandUnknownException : ApplicationException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandUnknownException" /> class,
+        /// with no parameters and message "Command unknown!"
+        /// </summary>
         public CommandUnknownException()
             : base("Command unknown!")
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandUnknownException" /> class,
+        /// with option for custom message!"
+        /// </summary>
+        /// <param name="message">custom message</param>
         public CommandUnknownException(string message)
             : base(message)
         {
         }
     }
-
-
 }
-
