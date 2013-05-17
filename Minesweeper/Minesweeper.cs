0bbab475c9d4643fc94db962b11d03b9e6eb0062
mariq88 <m.popova08@gmail.com>
2013-05-17 15:16:28 +0300

    Changed the class name from Program to Minesweeper

---
+++ Minesweeper/Minesweeper.cs
@@ -0,0 +1,15 @@
+﻿namespace MinesweeperProject
+{
+    using System;
+    using System.Linq;
+
+    public class Minesweeper
+    {
+        public static void Main(string[] args)
+        {
+            ConsoleMinesweeperGame game = new ConsoleMinesweeperGame(2,2,0);
+
+            game.Start();
+        }
+    }
+}
