4192082c4800048279028418497fa855c7183b4b
Hristo Bratanov <hristo_brat@yahoo.com>
2013-05-17 13:43:28 +0300

    testProj.proj added

---
+++ TestMinesweeperProject/TestConsoleMinesweeperGame.cs
@@ -0,0 +1,18 @@
+﻿namespace TestMinesweeperProject
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MinesweeperProject;
+    using System.Collections.Generic;
+
+
+    [TestClass]
+    public class TestConsoleMinesweeperGame
+    {
+        [TestMethod]
+        public void ParseCommand_WhenValidInput()
+        {
+
+        }
+    }
+}
