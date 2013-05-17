9b8ca830cd483e3978a029331b2a79e6e7a6f11d
m4r71n85 <martin85@abv.bg>
2013-05-17 12:38:13 +0300

    Update on unit tests references and MinesweeperCell tests

--- TestMinesweeperProject/TestMinesweeperCell.cs
+++ TestMinesweeperProject/TestMinesweeperCell.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinesweeperProject;
 
 namespace TestMinesweeperProject
 {
@@ -7,9 +8,50 @@ namespace TestMinesweeperProject
     public class TestMinesweeperCell
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCell_Empty_Constructor_IsRevealed()
         {
-            
+            MinesweeperCell cell = new MinesweeperCell();
+
+            Assert.AreEqual(false, cell.IsRevealed);
+        }
+
+        [TestMethod]
+        public void TestCell_Empty_Constructor_Value()
+        {
+            MinesweeperCell cell = new MinesweeperCell();
+
+            Assert.AreEqual(' ', cell.Value);
+        }
+
+        [TestMethod]
+        public void TestCell_Constructor_With_Params_Value()
+        {
+            MinesweeperCell cell = new MinesweeperCell('A', true);
+
+            Assert.AreEqual('A', cell.Value);
+        }
+
+        [TestMethod]
+        public void TestCell_Constructor_With_Params_IsRevealed()
+        {
+            MinesweeperCell cell = new MinesweeperCell('A', true);
+
+            Assert.AreEqual(true, cell.IsRevealed);
+        }
+
+        [TestMethod]
+        public void TestCellValue_Setter_Getter()
+        {
+            MinesweeperCell cell = new MinesweeperCell();
+            cell.Value = 'A';
+
+            Assert.AreEqual('A', cell.Value);
+        }
+
+        [TestMethod]
+        public void TestIsRevealed_Getter()
+        {
+
         }
     }
 }
