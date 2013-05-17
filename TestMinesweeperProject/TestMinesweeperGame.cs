d926e8e40fdc533396e6570e521c2c7e428bafec
Hristo Bratanov <hristo_brat@yahoo.com>
2013-05-17 14:18:28 +0300

    exceptions tests added

--- TestMinesweeperProject/TestMinesweeperGame.cs
+++ TestMinesweeperProject/TestMinesweeperGame.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 namespace TestMinesweeperProject
 {
     [TestClass]
-    public class TestMinesweeperGame
+    public class TestExceptions
     {
         [ExpectedException(typeof(NullReferenceException))]
         [TestMethod]
