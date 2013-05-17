5c806591ebb012be59ba41034fbdf5822a9ff6b8
Hristo Bratanov <hristo_brat@yahoo.com>
2013-05-17 11:54:08 +0300

    add unit test project

---
+++ Minesweeper/MinesweeperCell.cs
@@ -0,0 +1,39 @@
+﻿namespace MinesweeperProject
+{
+    using System;
+    using System.Linq;
+
+    public class MinesweeperCell
+    {
+        public MinesweeperCell()
+        {
+            this.Value = ' ';
+            this.IsRevealed = false;
+        }
+
+        public MinesweeperCell(char val, bool revealed)
+        {
+            this.Value = val;
+            this.IsRevealed = revealed;
+        }
+
+        public char Value { get; set; }
+
+        public bool IsRevealed { get; private set; }
+
+        public char GetCellValue
+        {
+            get
+            {
+                char result;
+                result = this.IsRevealed ? this.Value : '?';
+                return result;
+            }
+        }
+
+        public void Reveal()
+        {
+            this.IsRevealed = true;
+        }
+    }
+}
